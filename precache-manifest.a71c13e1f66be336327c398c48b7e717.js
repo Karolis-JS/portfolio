2f26603d3fe632236d80db3c3587e0c2b73b53be
unknown <karolis.soblinskas@gmail.com>
2021-05-12 09:50:11 +0300

    Updates

---
+++ precache-manifest.a71c13e1f66be336327c398c48b7e717.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "24c7e6d2c1b2d126ddca",
+    "url": "/portfolio/static/css/main.f4d9613b.chunk.css"
+  },
+  {
+    "revision": "24c7e6d2c1b2d126ddca",
+    "url": "/portfolio/static/js/main.2b1c6f68.chunk.js"
+  },
+  {
+    "revision": "65db28809bf8d983f6e3",
+    "url": "/portfolio/static/js/runtime~main.2388ca8f.js"
+  },
+  {
+    "revision": "8f01588819447ec561fe",
+    "url": "/portfolio/static/js/2.b7c450d8.chunk.js"
+  },
+  {
+    "revision": "9c093a58bd47bb7f5206",
+    "url": "/portfolio/static/js/3.246faef4.chunk.js"
+  },
+  {
+    "revision": "15260e77d60a5e3e18ff72a44d606649",
+    "url": "/portfolio/static/media/portret.15260e77.png"
+  },
+  {
+    "revision": "ed0f4221c7ac1913b5d80b8c1d947c0b",
+    "url": "/portfolio/static/media/mongodb.ed0f4221.png"
+  },
+  {
+    "revision": "9c97cbd4a6cad1b323e52fe9d58470a8",
+    "url": "/portfolio/static/media/recipe page.9c97cbd4.png"
+  },
+  {
+    "revision": "6a04082f5c9b7bd101e0d6fb545a4764",
+    "url": "/portfolio/static/media/rpg_game.6a04082f.png"
+  },
+  {
+    "revision": "7be795e356407089d3c555d872799af4",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c2ebb2d352e3412fb15fb0665ef86a83.js"
+  "/portfolio/precache-manifest.a71c13e1f66be336327c398c48b7e717.js"
 );
 
 workbox.clientsClaim();
