23713e40922461622cdfba3b95c75ab159e40538
unknown <karolis.soblinskas@gmail.com>
2021-05-12 14:39:17 +0300

    upd

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -25,7 +25,7 @@ function Portfolio() {
                     </a>
                 </div>
                 <div className="portfolio-card">
-                    <p>Project in progress...</p>
+                    <p>More app coming soon</p>
                     <img src="http://www.russellwilbinski.com/wp-content/uploads/2018/11/measuring_progress.jpg" alt=""/>
                     <a href="#">
                         <div className="goToCode">Go to code</div>
