2f44cbcf757e3b8a5b20eb17033331f58aff04ba
unknown <karolis.soblinskas@gmail.com>
2021-05-16 19:41:47 +0300

    Updates

---
+++ precache-manifest.adc8409cc57bf3e05a09f39454fe4a64.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "c00dc836d959ffb407bc",
+    "url": "/portfolio/static/css/main.ce97294f.chunk.css"
+  },
+  {
+    "revision": "c00dc836d959ffb407bc",
+    "url": "/portfolio/static/js/main.b82f26fe.chunk.js"
+  },
+  {
+    "revision": "07aa45f5d813c9dc2045",
+    "url": "/portfolio/static/js/runtime~main.385c2d7a.js"
+  },
+  {
+    "revision": "7bdaa960a12d5f73a930",
+    "url": "/portfolio/static/js/2.6c302e1b.chunk.js"
+  },
+  {
+    "revision": "393ab087ef9c8808a4c2",
+    "url": "/portfolio/static/js/3.931b5bf8.chunk.js"
+  },
+  {
+    "revision": "9c97cbd4a6cad1b323e52fe9d58470a8",
+    "url": "/portfolio/static/media/recipe page.9c97cbd4.png"
+  },
+  {
+    "revision": "6a04082f5c9b7bd101e0d6fb545a4764",
+    "url": "/portfolio/static/media/rpg_game.6a04082f.png"
+  },
+  {
+    "revision": "3b39063c71ff3af6a34d4fcad23957df",
+    "url": "/portfolio/static/media/rent.3b39063c.gif"
+  },
+  {
+    "revision": "3371fb41d08e6fdb63845ecc5351fd47",
+    "url": "/portfolio/static/media/portret.3371fb41.png"
+  },
+  {
+    "revision": "ed0f4221c7ac1913b5d80b8c1d947c0b",
+    "url": "/portfolio/static/media/mongodb.ed0f4221.png"
+  },
+  {
+    "revision": "e5334323e3271436a3e754314cd4c889",
+    "url": "/portfolio/static/media/CV Karolis Soblinskas.e5334323.pdf"
+  },
+  {
+    "revision": "15df06f1c1991f0def9a7e49dbbb8f85",
+    "url": "/portfolio/static/media/blog_page.15df06f1.png"
+  },
+  {
+    "revision": "65c919fb400953ae878cbb1dfa92250b",
+    "url": "/portfolio/static/media/mob.65c919fb.png"
+  },
+  {
+    "revision": "4fb12fa47234575aac3b3613ac36a7b1",
+    "url": "/portfolio/static/media/nuru_mobile.4fb12fa4.gif"
+  },
+  {
+    "revision": "b15c9a3d859cf746c040404c1014c645",
+    "url": "/portfolio/static/media/nurui_for_mobile.b15c9a3d.png"
+  },
+  {
+    "revision": "b3948f47c57332b79ea9a0a820737a69",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.908c980226ca25ae344dce3ac3812ac9.js"
+  "/portfolio/precache-manifest.adc8409cc57bf3e05a09f39454fe4a64.js"
 );
 
 workbox.clientsClaim();
