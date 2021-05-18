fb3a59b393674f3926a2ce65ca530c7d09b36c4f
unknown <karolis.soblinskas@gmail.com>
2021-05-18 17:39:47 +0300

    upd

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -14,40 +14,43 @@ function Contacts() {
             <div >
 
                 <div className="contact-location">
-                    <div className="wrapper">
+                    <div className="wrapper ">
                         <h1 className="glitch">CONTACT ME</h1>
                     </div>
-                    <div className="line"></div>
-                    <div>
-                        <div className="contact-info">
-                            <GoLocation className='location'/>
-                            <p>Vilnius, Lithuania</p>
+                    {/*<div className="line"></div>*/}
+                    <div className="contact-div">
+                        <div>
+                            <div className="contact-info">
+                                <GoLocation className='location'/>
+                                <p>Vilnius, Lithuania</p>
+                            </div>
+                            <div className="contact-info">
+                                <TiMail className='location'/>
+                                <p>karolis.soblinskas@gmail.com</p>
+                            </div>
                         </div>
-                        <div className="contact-info">
-                            <TiMail className='location'/>
-                            <p>karolis.soblinskas@gmail.com</p>
+                        {/*<div className="line"></div>*/}
+                        <div className="contact-icon">
+                            <p>
+                                <a href="https://www.facebook.com/karolis.soblinskas">
+                                    <SiFacebook className="fb"/>
+                                </a>
+                            </p>
+
+                            <p>
+                                <a href="https://www.linkedin.com/in/karolis-%C5%A1oblinskas-a98b53113/">
+                                    <AiFillLinkedin className="linkedin"/>
+                                </a>
+                            </p>
+                            <p>
+                                <a href="https://www.instagram.com/karolis_soblinskas/">
+                                    <GrInstagram className="instagram"/>
+                                </a>
+                            </p>
+
                         </div>
                     </div>
-                    <div className="line"></div>
-                    <div className="contact-icon">
-                        <p>
-                            <a href="https://www.facebook.com/karolis.soblinskas">
-                                <SiFacebook className="fb"/>
-                            </a>
-                        </p>
 
-                        <p>
-                            <a href="https://www.linkedin.com/in/karolis-%C5%A1oblinskas-a98b53113/">
-                                <AiFillLinkedin className="linkedin"/>
-                            </a>
-                        </p>
-                        <p>
-                            <a href="https://www.instagram.com/karolis_soblinskas/">
-                                <GrInstagram className="instagram"/>
-                            </a>
-                        </p>
-
-                    </div>
                 </div>
             </div>
 
